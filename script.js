2630819e45169179cda670672112c63f8f119831
Shrikanttathe-hub <142688445+Shrikanttathe-hub@users.noreply.github.com>
2023-10-30 01:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ let numberOfBooksRead  = 0 ;
 library.forEach((book) => {
     if(book.readingStatus) numberOfBooksRead ++;
 })
-//console.log(numberOfBooksRead);
+console.log(numberOfBooksRead);
 
 */
 
